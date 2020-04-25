89ca502e746942b9fbecf55e07e6b212c54b419b
Aakash <aakash.kumar0407@gmail.com>
2020-04-25 14:24:09 +0530

    final-version

--- app.js
+++ app.js
@@ -2,241 +2,310 @@ var express = require('express'),
     {google} = require('googleapis'),
     urlParser = require('url'),
     bodyParser = require('body-parser'),
+    session = require('express-session'),
     calendar = google.calendar("v3");
 
 
 
 var app = express()
-app.use(express.static('public'));
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.set('json spaces', 4)
-app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+app.use(session({
+    secret: "bokunoheroacademia_onepiece_naruto",
+    name: "sid",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { 
+        secure: false,
+        maxAge: 6000000
+    }
+}));
+
+const redirectLogin = (req, res, next) => {
+
+    if (!req.session.tokens) {
+        res.redirect('/')
+    } else {
+        next();
+    }
+
+}
+
+const redirectHome = (req, res, next) => {
+    
+
+    if (req.session.tokens) {
+       res.redirect('/profile')
+    } else {
+        next();
+    }
+
+
+}
 
 
 //Genrating an authentical URL
 
+function getOAuthClient(){
+    return new google.auth.OAuth2(
 
-const oauth2Client = new google.auth.OAuth2(
+    "668708568014-dkfj7i8vreg57ahtk9d3hf7u3vdbvsnv.apps.googleusercontent.com",
+    "NiXggkHTfV5wt1-2VgY616Bm",
+    "http://localhost:3000/google/authenticated/"
+)}
 
-	"668708568014-dkfj7i8vreg57ahtk9d3hf7u3vdbvsnv.apps.googleusercontent.com",
-	"NiXggkHTfV5wt1-2VgY616Bm",
-	"https://plannerc.herokuapp.com/google/authenticated/"
-)
+function getUrl(){
 
-const scopes = [
-  'Profile',
-  'https://www.googleapis.com/auth/calendar',
-  //'https://mail.google.com/'
-];
+    var oauth2Client =getOAuthClient();
+    const scopes = [
+        'Profile',
+        'https://www.googleapis.com/auth/calendar',
+        //'https://mail.google.com/'
+    ];
 
-const url = oauth2Client.generateAuthUrl({
+    const url = oauth2Client.generateAuthUrl({
 
-  access_type: 'offline',
-  scope: scopes
-});
+        access_type: 'offline',
+        scope: scopes
+    });
 
-
-
+    return url;
+}
 
 
 
-var events_global ;
-var name ;
-var picture_url ;
+app.get("/", redirectHome, async function(req, res) {
 
-app.get("/",async function(req,res){
-	res.render("login",{url:url});
+    var url = getUrl();
 
-  
-
+      res.render("login", {
+          url: url
+      });
 })
 
 
 //Getting Refresh Token
-app.get("/google/authenticated",function(req,res){
+app.get("/google/authenticated",function(req, res) {
 
-	var code = req.query.code;
-	console.log(code);
-	oauth2Client.getToken(code, async function (err, tokens) {
- 	
-  	if (!err) {
-    oauth2Client.setCredentials(tokens);
+  
+
+    var oauth2Client = getOAuthClient();
+    var session = req.session;
+    var code = req.query.code;
+    oauth2Client.getToken(code, async function(err, tokens) {
+      // Now tokens contains an access_token and an optional refresh_token. Save them.
+      if(!err) {
+        oauth2Client.setCredentials(tokens);
         var oauth2 = google.oauth2({
-  auth: oauth2Client,
-  version: 'v2'
-  });
-				 await oauth2.userinfo.get(
-      function(err, res) {
-    if (err) {
-       console.log(err);
-    } else {
-       var user  =res.data;
-       name =user['name'];
-       console.log(name);
-       picture_url = user['picture']
+                auth: oauth2Client,
+                version: 'v2'
+            });
+        var u = await oauth2.userinfo.get(
+                async function(err, user) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        var user = user.data;
+                        req.session.tokens=tokens;
+                        req.session.name=user['name'];  
+                        req.session.picture_url=user['picture'];
+                        req.session.save();
+                        res.redirect("/profile");
+
+                    }
+                });
+      }
+      else{
+      }
+    });
+
+})
+
+
+
+app.get("/profile",redirectLogin, async function(req, res) {
+
+       var oauth2Client = getOAuthClient();
+       oauth2Client.setCredentials(req.session["tokens"]);
+       var events = await listEvents(oauth2Client);
+       res.render("profile", {
+          events: events,
+          name: req.session.name,
+          picture_url: req.session.picture_url
+       });
+})
+
+
+app.get('/sendevent',redirectLogin, async function(req, res) {
+
+    var index = parseInt(req.query['id']);
+    var oauth2Client = getOAuthClient();
+    oauth2Client.setCredentials(req.session["tokens"]);
+    var events = await listEvents(oauth2Client);
+    res.render('sendevent', {
+        event: events[index],
+        name: req.session.name,
+        picture_url: req.session.picture_url
+    });
+
+})
+
+
+app.post('/sendevent', async function(req, res) {
+
+    var event = req.body.event;
+    var eventId = event['id'];
+    var newEmail = event['email'].split(",");
+    var oauth2Client = getOAuthClient();
+    oauth2Client.setCredentials(req.session["tokens"]);
+    var response = await sendInvite(oauth2Client, newEmail, eventId);
+    res.redirect('/profile')
+})
+
+app.get('/eventsjson',redirectLogin, async function(req, res) {
+    var oauth2Client = getOAuthClient();
+    oauth2Client.setCredentials(req.session["tokens"]);
+    var events = await listEvents(oauth2Client);
+    res.json(events);
+})
+
+
+app.get('/addevents',redirectLogin, function(req, res) {
+
+    res.render("addEvents", {
+        name: req.session.name,
+        picture_url: req.session.picture_url
+    });
+})
+app.post('/addevents', function(req, res) {
+
+    var oauth2Client = getOAuthClient();
+    oauth2Client.setCredentials(req.session["tokens"]);
+    res.render('addEvents', {
+        name: req.session.name,
+        picture_url: req.session.picture_url
+    });
+    var eventDetails = req.body.event;
+    var attendeDetails = req.body.event['email'].split(',');
+
+    var attendees = new Array();
+    for (var i = 0; i < attendeDetails.length; i++) {
+        var attende = new Object();
+
+        attende['email'] = attendeDetails[i];
+        attendees.push(attende);
     }
-  });
 
-
-
-
-        res.redirect("/profile");
-  			}
-	});
-})
-
-
-
-app.get("/profile",async function(req,res){
-
-  var events = await listEvents(oauth2Client);
-	res.render("profile",{events:events,name:name,picture_url:picture_url});
-  events_global = events;
-  
-  
-
-
-
-  
-})
-
-
-app.get('/sendevent',async function(req,res){
-
-  var index  = parseInt(req.query['id']);
-  res.render('sendevent',{event:events_global[index],name:name,picture_url:picture_url});
-
-})
-
-
-app.post('/sendevent',async function(req,res){
-
-  var event =req.body.event;
-  var eventId = event['id'];
-  var newEmail = event['email'].split(",");
-
-
-  var response = await sendInvite(oauth2Client,newEmail,eventId);
-  res.redirect('/profile')
-})
-
-app.get('/eventsjson',  async function(req,res){
-
-	res.json(events_global);
-})
-
-
-app.get('/addevents',function(req,res){
-
-		res.render("addEvents",{name:name,picture_url:picture_url});
-})
-app.post('/addevents',function(req,res){
-
-  res.render('addEvents',{name:name,picture_url:picture_url});
-  var eventDetails = req.body.event;
-  var attendeDetails = req.body.event['email'].split(',');
-  
-  var attendees = new Array();
-  for (var i = 0 ; i<attendeDetails.length;i++)
-  {
-      var attende  = new Object();
-
-      attende['email'] = attendeDetails[i];
-      attendees.push(attende);
-  }
-
-  console.log(Date.parse(eventDetails['start']));
-	var event = {
-    'summary':eventDetails['title'],
-    'description':eventDetails['body'],
-    'attendees':attendees,
-    'start':{
-      'dateTime' :eventDetails['start']+"T00:00:00+05:30"
-    },
-    'end':{
-      'dateTime':eventDetails['end']+"T24:00:00+05:30"
+    console.log(Date.parse(eventDetails['start']));
+    var event = {
+        'summary': eventDetails['title'],
+        'description': eventDetails['body'],
+        'attendees': attendees,
+        'start': {
+            'date': eventDetails['start']
+        },
+        'end': {
+            'date': eventDetails['end']
+        }
     }
-	}
-
-
-  addEvents(oauth2Client,event);
+    addEvents(oauth2Client, event);
 
 })
 
+app.get('/logout',redirectLogin, redirectLogin, function(req, res) {
+
+    req.session.destroy(err => {
+        if (err) {
+            res.redirect('/profile')
+        }
+
+        res.clearCookie('sid');
+        res.redirect('/');
+    })
 
 
- function addEvents(auth,event){
+});
+
+function addEvents(auth, event) {
 
 
-	calendar.events.insert({
-	  auth: auth	,
-	  calendarId: 'primary',
-	  resource: event,
-    sendNotifications:true
-	}, function(err, event) {
-	  if (err) {
-	    console.log('There was an error contacting the Calendar service: ' + err);
-	    return;
-	  }
-	  console.log('Event created: %s', event);
-	  console.log(event);
-	});
+    calendar.events.insert({
+        auth: auth,
+        calendarId: 'primary',
+        resource: event,
+        sendNotifications: true
+    }, function(err, event) {
+        if (err) {
+            console.log('There was an error contacting the Calendar service: ' + err);
+            return;
+        }
+    });
 
 }
 
 
 async function listEvents(auth) {
-  const calendar =  google.calendar({version: 'v3', auth});
+    const calendar = google.calendar({
+        version: 'v3',
+        auth
+    });
     const res = await calendar.events.list({
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 20,
-    singleEvents: true,
-    orderBy: 'startTime',
-  });
+        calendarId: 'primary',
+        timeMin: (new Date()).toISOString(),
+        maxResults: 20,
+        singleEvents: true,
+        orderBy: 'startTime',
+    });
     const events = res.data.items;
     return events;
 }
 
 
-async function sendInvite(auth ,newEmail,id) {
-    const calendar =  google.calendar({version: 'v3', auth});
+async function sendInvite(auth, newEmail, id) {
+    const calendar = google.calendar({
+        version: 'v3',
+        auth
+    });
     var res = await calendar.events.get({
-    calendarId: 'primary',
-    eventId:id
-    
-  });
+        calendarId: 'primary',
+        eventId: id
+
+    });
 
 
-    if(typeof(res.data['attendees'])=='undefined')
-    {
-      res.data['attendees'] = new Array();
-      console.log(res.data['attendees']);
+    if (typeof(res.data['attendees']) == 'undefined') {
+        res.data['attendees'] = new Array();
+        console.log(res.data['attendees']);
     }
-   for(var i=0;i<newEmail.length;i++)
-   {
-    res.data['attendees'].push({'email':newEmail[i]});
-   }
-   
- 
-  
-  var request = await calendar.events.patch({
-   
-   calendarId: 'primary',
-    eventId:id,
-      resource:{
-        
-        attendees :res.data['attendees']
-        
-      },
-      sendUpdates:'all'
-  })
+    for (var i = 0; i < newEmail.length; i++) {
+        res.data['attendees'].push({
+            'email': newEmail[i]
+        });
+    }
+
+
+
+    var request = await calendar.events.patch({
+
+        calendarId: 'primary',
+        eventId: id,
+        resource: {
+            attendees: res.data['attendees']
+        },
+        sendUpdates: 'all'
+    })
 }
 
 
 
 
-app.listen(process.env.IP||3000,function(){
-	console.log("Server Started");
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server Started");
 })
